e6558acdd6643119a0db82bd5aebedc597be2976
ricardocremonese <173083816+ricardocremonese@users.noreply.github.com>
2025-08-19 23:00:49 -0300

    feat(auth): atualizar layout e imagem da tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import { AuthGate } from "@/components/auth/AuthGate";
 import Dashboard from "./components/Dashboard";
 import Loja from "./components/Loja";
 import Estoque from "./components/Estoque";
@@ -20,18 +21,20 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/loja" element={<Loja />} />
-            <Route path="/estoque" element={<Estoque />} />
-            <Route path="/assistencia" element={<AssistenciaTecnica />} />
-            <Route path="/locacao" element={<div className="p-8 text-center text-gray-600">Módulo de Locação em desenvolvimento...</div>} />
-            <Route path="/contabilidade" element={<Contabilidade />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
+        <AuthGate>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/loja" element={<Loja />} />
+              <Route path="/estoque" element={<Estoque />} />
+              <Route path="/assistencia" element={<AssistenciaTecnica />} />
+              <Route path="/locacao" element={<div className="p-8 text-center text-gray-600">Módulo de Locação em desenvolvimento...</div>} />
+              <Route path="/contabilidade" element={<Contabilidade />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </AuthGate>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
